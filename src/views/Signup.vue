bd68c356a295c0a06f6323b9b37d8ab34ddfc00b
b4ssed <matheus.sombrio@hotmail.com>
2022-12-03 15:54:07 -0300

    Cadastro de usuário

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,10 +7,8 @@
     </div>
   </div>
   <main class="main-content mt-0">
-    <div
-      class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
-      style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;"
-    >
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
+      style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
@@ -28,17 +26,23 @@
               <h5>Cadastre-se</h5>
             </div>
             <div class="card-body">
-              <form role="form">
-                <argon-input type="text" placeholder="Nome" aria-label="Name" />
-                <argon-input type="email" placeholder="Email" aria-label="Email" />
-                <argon-input type="date" placeholder="Nascimento" aria-label="Data de Nascimento"/>
-                <argon-input type="password" placeholder="Senha" aria-label="Password"/>
-                <div class="row">
+              <form role="form" v-on:submit.prevent="createUser(this.user)">
+                <input class="form-control mt-3" type="text" placeholder="Nome" v-model="user.nome" aria-label="Name" />
+                <input class="form-control mt-3" type="email" placeholder="Email" v-model="user.email" aria-label="Email" />
+                <input class="form-control mt-3" type="date" placeholder="Nascimento" v-model="user.data_nascimento" aria-label="Data de Nascimento"/>
+                <input class="form-control mt-3" type="password" placeholder="Senha" v-model="user.senha" aria-label="Password" />
+                <div class="row mt-3">
                   <div class="col-6">
-                    <argon-radio name="F" checked="checked">Feminino</argon-radio>
+                    <input value="M" v-model="user.sexo" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                    <label class="form-check-label" for="flexRadioDefault1">
+                      Masculino
+                    </label>
                   </div>
                   <div class="col-6">
-                    <argon-radio name="M">Masculino</argon-radio>
+                    <input type="radio" value="F" v-model="user.sexo" name="flexRadioDefault" id="flexRadioDefault2">
+                    <label class="form-check-label" for="flexRadioDefault2">
+                      Feminino
+                    </label>
                   </div>
                 </div>
                 <div class="text-center">
@@ -46,10 +50,7 @@
                 </div>
                 <p class="text-sm mt-3 mb-0">
                   Já possui uma conta?
-                  <a
-                    href="Signin"
-                    class="text-dark font-weight-bolder"
-                  >Entre</a>
+                  <a href="Signin" class="text-dark font-weight-bolder">Entre</a>
                 </p>
               </form>
             </div>
@@ -62,10 +63,12 @@
 </template>
 
 <script>
+import axios from "axios";
+
 import Navbar from "@/examples/PageLayout/Navbar.vue";
 import AppFooter from "@/examples/PageLayout/Footer.vue";
-import ArgonInput from "@/components/ArgonInput.vue";
-import ArgonRadio from "@/components/ArgonRadio.vue";
+// import ArgonInput from "@/components/ArgonInput.vue";
+// import ArgonRadio from "@/components/ArgonRadio.vue";
 import ArgonButton from "@/components/ArgonButton.vue";
 const body = document.getElementsByTagName("body")[0];
 
@@ -74,9 +77,20 @@ export default {
   components: {
     Navbar,
     AppFooter,
-    ArgonInput,
     ArgonButton,
-    ArgonRadio
+  },
+  data() {
+    return {
+      user: {
+        id_usuario: '',
+        email: '',
+        data_nascimento: '',
+        sexo: '',
+        nome: '',
+        senha: '',
+        isAdmin: false
+      }
+    }
   },
   created() {
     this.$store.state.hideConfigButton = true;
@@ -92,5 +106,19 @@ export default {
     this.$store.state.showFooter = true;
     body.classList.add("bg-gray-100");
   },
+  methods: {
+    createUser(user) {
+      console.log(user)
+      axios
+        .post("http://localhost:8888/users", user)
+        .then(response => {
+          console.log(response)
+          this.$router.push('/Signin')
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  }
 };
 </script>
